8d5d47cb4027a0a8c692b38855c3efa8abcf020f
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-11 23:42:17 +0100

    Create Bash init.sh

---
+++ Bash init.sh
@@ -0,0 +1,8 @@
+# Clone the Homestead repository
+git clone https://github.com/laravel/homestead.git ~/Homestead
+# Checkout the release branch
+cd ~/Homestead
+git checkout release
+# Initialize the Homestead configuration
+bash init.sh # For macOS/Linux
+init.bat # For Windows
