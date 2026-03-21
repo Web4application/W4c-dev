b6d8f0c0d536d17ed8f3dd88cf4729158a068681
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-21 21:56:49 +0100

    Update and rename bash.sh to Caddyfile/bash.sh

--- Caddyfile/bash.sh
+++ Caddyfile/bash.sh
@@ -1,3 +1,6 @@
+brew install mkcert nss   # nss for Firefox if needed
+mkcert -install
+mkcert web4.dev "*.web4.dev" localhost 127.0.0.1
 # From repo root
 # Option A: Keep one (e.g. locallhost) and delete the duplicates
 rm -rf Localhost/ localhost.co/
