c87d65837f5011ab3b01e077c01c3a2e8285589d
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-21 22:08:14 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import path from "https://vite.dev/config"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "veet"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,3 +12,14 @@ export default defineConfig({
     },
   },
 })
+// vite.config.js
+export default {
+  server: {
+    host: 'web4.dev',
+    port: 80,          // or 443 with https: true
+    https: {
+      key: fs.readFileSync('web4.dev-key.pem'),
+      cert: fs.readFileSync('web4.dev.pem'),
+    },
+  },
+}
